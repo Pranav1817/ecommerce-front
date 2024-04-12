efe2ed875add9820c5f3365f2a29ccca2d0a3457
Chaitanya637 <87860944+Pranav1817@users.noreply.github.com>
2024-04-12 22:29:22 +0530

    first commit

---
+++ components/ProductBox.js
@@ -0,0 +1,84 @@
+import styled from "styled-components";
+import Button from "@/components/Button";
+import CartIcon from "@/components/icons/CartIcon";
+import Link from "next/link";
+import {useContext} from "react";
+import {CartContext} from "@/components/CartContext";
+import Image from "next/image";
+const ProductWrapper = styled.div`
+  
+`;
+
+const WhiteBox = styled(Link)`
+  background-color: #fff;
+  padding: 20px;
+  height: 120px;
+  text-align: center;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-radius: 10px;
+  img{
+    max-width: 100%;
+    max-height: 80px;
+  }
+`;
+
+const Title = styled(Link)`
+  font-weight: normal;
+  font-size:.9rem;
+  color:inherit;
+  text-decoration:none;
+  margin:0;
+`;
+
+const ProductInfoBox = styled.div`
+  margin-top: 5px;
+`;
+
+const PriceRow = styled.div`
+  display: block;
+  @media screen and (min-width: 768px) {
+    display: flex;
+    gap: 5px;
+  }
+  align-items: center;
+  justify-content:space-between;
+  margin-top:2px;
+`;
+
+const Price = styled.div`
+  font-size: 1rem;
+  font-weight:400;
+  text-align: right;
+  @media screen and (min-width: 768px) {
+    font-size: 1.2rem;
+    font-weight:600;
+    text-align: left;
+  }
+`;
+
+export default function ProductBox({_id,name,description,price,image}) {
+  const {addProduct} = useContext(CartContext);
+  const url = '/product/'+_id;
+  return (
+    <ProductWrapper>
+      <WhiteBox href={url}>
+        <div>
+          <Image src="/dummy.jpeg" alt="dummy image"  width={100} height={100}/>
+        </div>
+      </WhiteBox>
+      <ProductInfoBox>
+        <Title href={url}>{name}</Title>
+        <PriceRow>
+          <Price>
+            ${price}
+          </Price>
+          <Button block onClick={() => addProduct(_id)} primary outline>
+            Add to cart
+          </Button>
+        </PriceRow>
+      </ProductInfoBox>
+    </ProductWrapper>
+  );
+}
